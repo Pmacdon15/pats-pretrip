be6ec48048b15e3b84187ed8b223a9509c47178d
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-19 15:49:17 -0600

    layout for app bar

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import AppBar from '../appBar';
+export default function Home() {
+    return (
+        <>
+        <AppBar />
+        {/* <div> Pat's Pre-Trips Home Page</div> */}
+        </>
+    )
+}
