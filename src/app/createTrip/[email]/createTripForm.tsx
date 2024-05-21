70bd0f9a893bf56c1f59e8b5651f7145cc7a5875
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-20 20:55:17 -0600

    more layout complete for create trip page

--- src/app/createTrip/[email]/createTripForm.tsx
+++ src/app/createTrip/[email]/createTripForm.tsx
@@ -6,19 +6,101 @@ import styles from './page.module.css';
 export default function CreateTripForm() {
     return (
         <>
-        <div className={styles.textInputContainer} >
-            <TextField className={styles.textField} id="standard-basic" label="Carrier" variant="standard" />
-            <TextField className={styles.textField} id="standard-basic" label="Address" variant="standard" />
-            <TextField className={styles.textField} id="standard-basic" label="Carrier" variant="standard" />
-            <div className={styles.dateTimeContainer}>
-            <TextField className={styles.dateTimeField} id="standard-basic" label="Date" variant="standard" />
-            <TextField className={styles.dateTimeField} id="standard-basic" label="Time(24hr)" variant="standard" />            
+            <div className={styles.textInputContainer} >
+                <TextField className={styles.textField} id="standard-basic" label="Carrier" variant="standard"
+                    sx={{
+                        "& .MuiInputLabel-root": {
+                            color: "white",
+                            "&.Mui-focused": {
+                                color: "white",
+                            },
+                        },
+                        "& .MuiInput-underline": {
+                            "&:before": {
+                                borderBottomColor: "white",
+                            },
+                            "&:after": {
+                                borderBottomColor: "#F7F7F7",
+                            },
+                        },
+                    }}
+
+                />
+                <TextField className={styles.textField} id="standard-basic" label="Address" variant="standard"
+                    sx={{
+                        "& .MuiInputLabel-root": {
+                            color: "white",
+                            "&.Mui-focused": {
+                                color: "white",
+                            },
+                        },
+                        "& .MuiInput-underline": {
+                            "&:before": {
+                                borderBottomColor: "white",
+                            },
+                            "&:after": {
+                                borderBottomColor: "#F7F7F7",
+                            },
+                        },
+                    }} />
+                <TextField className={styles.textField} id="standard-basic" label="Carrier" variant="standard"
+                    sx={{
+                        "& .MuiInputLabel-root": {
+                            color: "white",
+                            "&.Mui-focused": {
+                                color: "white",
+                            },
+                        },
+                        "& .MuiInput-underline": {
+                            "&:before": {
+                                borderBottomColor: "white",
+                            },
+                            "&:after": {
+                                borderBottomColor: "#F7F7F7",
+                            },
+                        },
+                    }} />
+                <div className={styles.dateTimeContainer}>
+                    <TextField className={styles.dateTimeField} id="standard-basic" label="Date" variant="standard"
+                        sx={{
+                            "& .MuiInputLabel-root": {
+                                color: "white",
+                                "&.Mui-focused": {
+                                    color: "white",
+                                },
+                            },
+                            "& .MuiInput-underline": {
+                                "&:before": {
+                                    borderBottomColor: "white",
+                                },
+                                "&:after": {
+                                    borderBottomColor: "#F7F7F7",
+                                },
+                            },
+                        }} />
+                    <TextField className={styles.dateTimeField} id="standard-basic" label="Time(24hr)" variant="standard"
+                        sx={{
+                            "& .MuiInputLabel-root": {
+                                color: "white",
+                                "&.Mui-focused": {
+                                    color: "white",
+                                },
+                            },
+                            "& .MuiInput-underline": {
+                                "&:before": {
+                                    borderBottomColor: "white",
+                                },
+                                "&:after": {
+                                    borderBottomColor: "#F7F7F7",
+                                },
+                            },
+                        }} />
+                </div>
+
             </div>
-            
-        </div>
-        <div>
-        <FormControlLabel control={<Checkbox />} label="Label" />
-        </div>
-        </> 
+            <div>
+                <FormControlLabel control={<Checkbox sx={{color:"white"}}/>} label="Label" sx={{color:"white"}} />
+            </div>
+        </>
     )
 }
