9fd3d954c19ede3dac4b32a2f4076656850e71ea
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-31 00:32:10 -0600

    State is properly passed through the url

--- src/app/currentTrips/[email]/page.tsx
+++ src/app/currentTrips/[email]/page.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect, useCallback } from 'react';
 import { Button } from '@mui/material';
 import AddDefect from "./addDefect";
 import { useSearchParams } from 'next/navigation'
-import { useRouter ,usePathname} from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 
 type Trip = {
@@ -37,39 +37,46 @@ export default function CurrentTrips({ params }: { params: { email: string } }) 
     const pathname = usePathname()
     const searchParams = useSearchParams()
     const tripId = Number(searchParams.get("tripId")) ?? 0;
-    const truckId = Number(searchParams.get("truckId")) ?? 0;
-   
+    // const truckId = Number(searchParams.get("truckId")) ?? 0;
+
     const decodedEmail = decodeURIComponent(params.email);
     const [trips, setTrips] = useState<Trip[]>([]);
     const [trucks, setTrucks] = useState<Truck[]>([]);
     const [defects, setDefects] = useState<any[]>([]);
     const [selectedTrip, setSelectedTrip] = useState<number>(0);
-    const [selectedTruck, setSelectedTruck] = useState<number>(0);
+    // const [selectedTruck, setSelectedTruck] = useState<number>(0);
 
     useEffect(() => {
         const fetchData = async () => {
             const fetchedTrips = await getCurrentTrips(decodedEmail) as Trip[];
             const fetchedTrucks = await getCurrentTrucksInfo(decodedEmail) as Truck[];
             const fetchedDefects = await getCurrentDefects(decodedEmail) as any;
-            setDefects(fetchedDefects);
 
             setTrips(fetchedTrips);
             setTrucks(fetchedTrucks);
-            setSelectedTrip(tripId);
-            setSelectedTruck(truckId);
-            router.push(pathname + '?' + createQueryString(tripId, truckId));
+            setDefects(fetchedDefects);  
+            
+            if (fetchedTrips.length > 0 && tripId === 0) {
+                setSelectedTrip(fetchedTrips[0].id); // Set selectedTrip to the ID of the first trip
+                console.log("Trips: ", trips);
+                console.log("Selected Trip: ", selectedTrip);
+                router.push(pathname + '?' + createQueryString(fetchedTrips[0].id));
+            }
         };
         fetchData();
     }, [decodedEmail]);
 
+    // useEffect(() => {
+    //     setSelectedTrip(8)        
+    //     router.push(pathname + '?' + createQueryString(selectedTrip));
+    // }, [trips]);
+
     const handleTripClick = (trip: Trip, truck: Truck) => {
-        setSelectedTrip(tripId);
-        setSelectedTruck(truckId);
-        //console.log("Selected Trip: ", trip.id);
-        router.push(pathname + '?' + createQueryString(trip.id, truck.id));
+        setSelectedTrip(tripId);;
+        router.push(pathname + '?' + createQueryString(trip.id));
     };
 
-   // const filteredDefects = defects.filter(defect => defect.tripid === selectedTrip?.id);
+    // const filteredDefects = defects.filter(defect => defect.tripid === selectedTrip?.id);
 
     const [showAddDefect, setShowAddDefect] = useState(false);
 
@@ -83,10 +90,10 @@ export default function CurrentTrips({ params }: { params: { email: string } }) 
     };
 
     const createQueryString = useCallback(
-        (tripId: number, truckId: number) => {
+        (tripId: number) => {
             const params = new URLSearchParams(searchParams.toString())
             params.set('tripId', tripId.toString())
-            params.set('truckId', truckId.toString())
+            //params.set('truckId', truckId.toString())
             return params.toString()
         },
         [searchParams]
@@ -96,7 +103,7 @@ export default function CurrentTrips({ params }: { params: { email: string } }) 
         <>
             {/* Button is nested for styles reasons */}
             <BasicDisplayTrips onTripClick={handleTripClick} trips={trips} trucks={trucks} />
-            <InDepthDisplayTrip trips={trips}  trucks={trucks}  defects={defects} selectedTripId={tripId}>
+            <InDepthDisplayTrip trips={trips} trucks={trucks} defects={defects} selectedTripId={tripId}>
                 <Button variant="contained" color="primary" onClick={handleAddDefectClick}>
                     Add Defect
                 </Button>
