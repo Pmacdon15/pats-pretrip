3310b202baeca9f883ba6b63150f8f74508926bd
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-05-31 18:55:57 -0600

    fixed sign up page css

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -3,9 +3,14 @@ import SignUpForm from './signUpForm';
 import LoginSignUpContainer from '../containers/loginSignUpContainer';
 export default function SignUp() {
     return (
-        <LoginSignUpContainer>
-            <h1 className={styles.title}>Sign Up</h1>
-            <SignUpForm />
-        </LoginSignUpContainer>
+        <>
+            <div className={styles.title}>
+                <h1>Pat&apos;s Pre-Trip</h1>
+            </div>
+            <LoginSignUpContainer>
+                <h1 className={styles.title}>Sign Up</h1>
+                <SignUpForm />
+            </LoginSignUpContainer>
+        </>
     );
 }
