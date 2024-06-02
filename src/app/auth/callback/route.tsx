16ee47e51711320ca0f36cbc3d4d54c6ae90a806
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-01 18:08:43 -0600

    test

--- src/app/auth/callback/route.tsx
+++ src/app/auth/callback/route.tsx
@@ -1,3 +1,3 @@
 import { handleAuth, getUser } from '@workos-inc/authkit-nextjs';
 
-export const GET = handleAuth({ returnPathname: '/' });
+export const GET = handleAuth({ returnPathname: 'https://pats-pretrip.vercel.app/' });
