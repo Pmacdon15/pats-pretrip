a883b4f57e3bc63ac3dad3974434d089510a670e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-02 13:33:43 -0600

    fixed build errors

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -17,10 +17,10 @@ export default async function Home() {
         <div className={styles.mainPage}>
             <h1 className={styles.header}>Welcome to the Pat&apos;s Pre-Trips</h1>
             <div className={styles.infoContainer}>
-                Pat's Pre Trips
+                Pat&apos;s Pre Trips
                 Streamline pre-trip inspections with this innovative web app, built with Next.js, Auth Kit, Vercel, and TypeScript. Quickly conduct safety checks and hit the road with confidence!
                 Developed by Patrick MacDonald
-                Let me know if you'd like me to make any changes!
+                Let me know if you&apos;d like me to make any changes!
             </div>
             <div className={styles.infoContainer}>
                 <UserStats />
