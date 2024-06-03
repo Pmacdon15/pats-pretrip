0af3e8afd89545036d8fe8d31d1a38b76b8d45b0
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-02 22:10:38 -0600

    test on production playing with time stamps

--- src/app/containers/trips/basicDisplayTrips.tsx
+++ src/app/containers/trips/basicDisplayTrips.tsx
@@ -33,16 +33,21 @@ type Trip = {
   }) {    
     return (
       <div className={styles.tripsBasicInfoContainer}>
-        {trips?.map((trip, index) => (
-          <div
-            className={styles.tripsBasicInfo}
-            key={index}
-            onClick={() => onTripClick(trip, trucks[index])} // Call the onTripClick handler when a trip is clicked
-          >
-            Vehicle: {trucks && trucks[index]?.trucklp}, <br />
-            {trip.datetime.toLocaleString()}
-          </div>
-        ))}
+      {trips?.map((trip, index) => (
+        <div
+        className={styles.tripsBasicInfo}
+        key={index}
+        onClick={() => onTripClick(trip, trucks[index])} // Call the onTripClick handler when a trip is clicked
+        >
+        Vehicle: {trucks && trucks[index]?.trucklp}, <br /><br/>
+        {new Date(trip.datetime.getTime() + 6 * 60 * 60 * 1000).toString()} {/* Add 6 hours to the trip datetime */} <br/><br/>
+        input Date:
+        {trip.inputdate.toString()}
+        <br /> <br/>
+        input date + 6 hours:
+        {new Date(trip.inputdate.getTime() + 6 * 60 * 60 * 1000).toString()}
+        </div>
+      ))}
       </div>
     );
   }
