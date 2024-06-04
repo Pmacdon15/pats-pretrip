bdc3028d675b98f6d64c9f7919871eba4ba6fedc
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-03 18:24:02 -0600

    fixed sign out bug, I can now control the redirect after sign out

--- src/app/appBar/loginLogoutButtons.tsx
+++ src/app/appBar/loginLogoutButtons.tsx
@@ -1,15 +1,24 @@
-import { getUser, getSignInUrl, signOut } from '@workos-inc/authkit-nextjs';
-import middleware from '../../middleware';
+import { getUser, getSignInUrl } from '@workos-inc/authkit-nextjs';
+// import middleware from '../../middleware';
 import SignInButton from './signInButton';
 import { Button } from '@mui/material';
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
 
-export default async function LogInLogOuButtons(){
+async function signOut() {
+    cookies().delete('wos-session');
+    redirect('/');
+}
+
+export default async function LogInLogOuButtons() {
+    'use server'
     const { user } = await getUser();
-    const signInUrl = await getSignInUrl();
-    return(
-    user ? (<form action={async () => {
-        'use server';
-        await signOut();
-    }}><Button type="submit" color="inherit">Logout</Button></form>) : (<SignInButton signInUrl={signInUrl} />)
+    const signInUrl = await getSignInUrl();    
+   
+    return (
+        user ? (<form action={async () => {
+            'use server';
+            await signOut();
+        }}><Button type="submit" color="inherit">Logout</Button></form>) : (<SignInButton signInUrl={signInUrl} />)
     )
 }
