3702c4f0ea27f945dcaece28d5ef9d2d41d56783
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-06 19:40:16 -0600

    Fade in and out transitions added to add defect panel as well as parameters added to the on hide function so that it does not load when the cancel button is clicked

--- src/app/currentTrips/[email]/addDefect.tsx
+++ src/app/currentTrips/[email]/addDefect.tsx
@@ -4,29 +4,22 @@ import TruckCheckBoxesForForm from '@/app/createTrip/[email]/truckCheckBoxesForF
 import { addDefect, changeToMajorDefect, addRemark } from '@/app/db';
 import { useEffect, useState } from 'react';
 
-
-export default function AddDefect({  tripId, defects, onHide }: ({ tripId: number, defects: any, onHide: () => void })) {    
+export default function AddDefect({ tripId, defects, onHide }: ({ tripId: number, defects: any, onHide:(formSubmitted: boolean)  => void })) {
     const [show, setShow] = useState(false);
 
-    useEffect(() => {
-        // When component mounts, add a delay before showing to create a transition effect
-        const timer = setTimeout(() => {
-            setShow(true);
-        }, 100); // Adjust the delay time as needed
-
-        return () => clearTimeout(timer); // Clean up the timer on unmount
+    useEffect(() => {        
+        setShow(true);        
     }, []);
-    
-    
-    const handleSubmit = async  (event: React.FormEvent<HTMLFormElement>) => {
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const form = event.currentTarget;
         const formData = new FormData(form);
         //console.log("tripId to be updated: ", tripId);
-                Array.from(formData.entries()).forEach(([key, value]) => {
+        Array.from(formData.entries()).forEach(([key, value]) => {
             if (value === "on" && !key.endsWith("M")) {
                 const has_m_defect = formData.get(`${key}M`) === "on";
-                const selectedDefects = defects.filter((defect: any) => defect.tripid === tripId);                
+                const selectedDefects = defects.filter((defect: any) => defect.tripid === tripId);
                 const existingDefect = selectedDefects.find((defect: any) => defect.name === key);
                 if (!existingDefect) {
                     addDefect(tripId, key, has_m_defect);
@@ -39,19 +32,27 @@ export default function AddDefect({  tripId, defects, onHide }: ({ tripId: numbe
         });
         const newRemarks = formData.get("remarks") as string;
         await addRemark(tripId, newRemarks);
+
         setShow(false);
-        onHide();
+        setTimeout(() => {
+            onHide(true);
+        }, 1000);
     };
 
     return (
-        <form onSubmit={handleSubmit} className={`${styles.container} ${show ? styles.show : ''}`}>
+        <form onSubmit={handleSubmit} className={`${styles.container} ${show ? styles.show : styles.hide}`}>
             Add Defect:
             <TruckCheckBoxesForForm />
             <Button variant="contained" type="submit">
                 Submit
             </Button>
             <br />
-            <Button variant="contained" onClick={onHide}>
+            <Button variant="contained"  onClick={() => {
+                setShow(false);
+                setTimeout(() => {
+                    onHide(false);
+                }, 1000);
+            }}>
                 Cancel
             </Button>
         </form>
